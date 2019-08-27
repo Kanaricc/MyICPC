a41dc998c3227a79f9b9288c0d3ae37b63e535dc
Kanari <iovo7c@gmail.com>
2019-08-27 20:21:03 +0800

    计算几何模板更新

--- test.cpp
+++ test.cpp
@@ -1,91 +1,117 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <cstring>
+#include <vector>
+#include <queue>
 using namespace std;
-typedef long long ll;
-const int MAXN=100010;
-const int P=1e9+7;
-struct Node{
-    char chr;
-    int cnt[MAXN];
-    bool operator<(const Node &b)const{
-        for(int i=MAXN-1;i>=0;i--){
-            if(cnt[i]!=b.cnt[i])return cnt[i]<b.cnt[i];
-        }
-        return false;
-    }
-}nodes[30];
-bool canzero[30];
-bool vis[30];
-int weight[30];
-vector<string> str;
+using ll = long long;
+const int MAXV = 50010, MAXE = 50010;
+const int MAXQ = 50050;
+struct Edge
+{
+    int v;
+    int w;
+} edges[MAXE];
 
-void init(){
-    str.clear();
-    memset(weight,0,sizeof(weight));
-    memset(vis,0,sizeof(vis));
-    memset(canzero,1,sizeof(canzero));
-    for(int i=0;i<26;i++){
-        nodes[i].chr='a'+i;
-        memset(nodes[i].cnt,0,sizeof(nodes[i].cnt));
+int idx = 0;
+vector<int> g[MAXV];
+inline void adde(int u, int v, int w)
+{
+    edges[++idx].v = v;
+    edges[idx].w = w;
+    g[u].push_back(idx);
+}
+
+struct Node
+{
+    ll dist;
+    int u;
+    int nexte;
+
+    Node(int u, ll dist, int nexte) : u(u), dist(dist), nexte(nexte) {}
+
+    bool operator<(const Node &b) const
+    {
+        return dist > b.dist;
+    }
+};
+int vlen, elen, qlen;
+ll ans[MAXQ];
+
+void solve()
+{
+    priority_queue<Node> pq;
+    for (int u = 1; u <= vlen; u++)
+    {
+        for (int ei = 0; ei < min((size_t)1, g[u].size()); ei++)
+        {
+            Edge &e = edges[g[u][ei]];
+            pq.push(Node(u, e.w, ei));
+        }
+    }
+    int kth = 0;
+    while (!pq.empty() && kth<=5e4+10)
+    {
+        Node cur = pq.top();
+        ans[++kth]=cur.dist;
+        pq.pop();
+
+        Edge &e = edges[g[cur.u][cur.nexte]];
+        //expand next v
+        Node next = cur;
+        next.u = e.v;
+        next.nexte = 0;
+        bool hasnext = 0;
+        if (next.nexte < g[next.u].size())
+        {
+            next.dist += edges[g[next.u][next.nexte]].w;
+            pq.push(next);
+        }
+
+        bool hascur = 0;
+        // no edge to expand current v
+        if (cur.nexte + 1 < g[cur.u].size())
+        {
+            Edge &ne = edges[g[cur.u][cur.nexte + 1]];
+            cur.dist -= e.w;
+            cur.dist += ne.w;
+            cur.nexte++;
+            pq.push(cur);
+        }
     }
 }
 
-inline void countstr(string str){
-    reverse(str.begin(),str.end());
-    for(int i=0;i<str.size();i++){
-        nodes[str[i]-'a'].cnt[i]++;
-        vis[str[i]-'a']=1;
+int main()
+{
+    int kase;
+    scanf("%d",&kase);
+    while (kase--)
+    {
+        idx = 0;
+        scanf("%d%d%d",&vlen,&elen,&qlen);
+        for (int i = 0; i <= vlen; i++)
+            g[i].clear();
+
+        for (int i = 1; i <= elen; i++)
+        {
+            int u, v, w;
+            scanf("%d%d%d",&u,&v,&w);
+            adde(u, v, w);
+        }
+        for (int i = 1; i <= vlen; i++)
+        {
+            sort(g[i].begin(), g[i].end(), [](int a, int b) {
+                return edges[a].w < edges[b].w;
+            });
+        }
+        solve();
+
+        for (int i = 1; i <= qlen; i++)
+        {
+            int q;
+            scanf("%d",&q);
+            printf("%lld\n",ans[q]);
+        }
     }
-}
-int main(){
-    ios::sync_with_stdio(false);
-    int n;
-    int kase=0;
-    while(cin>>n){
-        init();
-
-        for(int i=0;i<n;i++){
-            string inp;cin>>inp;
-            str.push_back(inp);
-            if(inp.size()>1)canzero[inp[0]-'a']=false;
-            countstr(inp);
-        }
-        for(int i=0;i<26;i++){
-            for(int j=0;j<MAXN-1;j++){
-                nodes[i].cnt[j+1]+=nodes[i].cnt[j]/26;
-                nodes[i].cnt[j]%=26;
-            }
-        }
-
-        sort(nodes,nodes+26);
-        int zero=0;
-        for(zero=0;zero<26;zero++){
-            if(canzero[nodes[zero].chr-'a'])break;
-        }
-
-        bool needzero=false;
-        if(vis[nodes[0].chr-'a'])needzero=1;
-
-        int curw=25;
-        for(int i=25;i>=0;i--){
-            if(needzero && i==zero)continue;
-            if(!vis[nodes[i].chr-'a'])continue;
-            weight[nodes[i].chr-'a']=curw;
-            curw--;
-        }
-        //calculate answer
-        ll ans=0;
-        for(int i=0;i<n;i++){
-            ll one=0;
-            for(int j=0;j<str[i].size();j++){
-                one=((one*26)%P+weight[str[i][j]-'a'])%P;
-            }
-            ans=(ans+one)%P;
-        }
-        cout<<"Case #"<<++kase<<": "<<ans<<endl;
-    }
-
     return 0;
 }
