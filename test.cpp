96d2f976cf81014cb50f8b6a90bbb132c7f09476
Kanari <iovo7c@gmail.com>
2019-08-28 16:35:47 +0800

    a shaifa

--- test.cpp
+++ test.cpp
@@ -1,117 +1,62 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 #include <vector>
-#include <queue>
+#include <algorithm>
 using namespace std;
-using ll = long long;
-const int MAXV = 50010, MAXE = 50010;
-const int MAXQ = 50050;
-struct Edge
-{
-    int v;
-    int w;
-} edges[MAXE];
+using ll=long long;
+const int MAXN=50010;
 
-int idx = 0;
-vector<int> g[MAXV];
-inline void adde(int u, int v, int w)
-{
-    edges[++idx].v = v;
-    edges[idx].w = w;
-    g[u].push_back(idx);
+bool isn_p[MAXN];
+vector<int> primes;
+int mu[MAXN];
+int premu[MAXN];
+void init_prime(int len){
+    isn_p[1]=1;
+    mu[1]=1;
+    for(int i=2;i<=len;i++){
+        if(!isn_p[i]){
+            primes.push_back(i);
+            mu[i]=-1;
+        }
+
+        for(int j=0;j<primes.size() && i*primes[j]<=len;j++){
+            isn_p[i*primes[j]]=1;
+            mu[i*primes[j]]=mu[i]*-1;
+
+            if(i%primes[j]==0){
+                mu[i*primes[j]]=0;
+                break;
+            }
+        }
+    }
+    premu[0]=0;
+    for(int i=1;i<=len;i++)premu[i]=premu[i-1]+mu[i];
 }
 
-struct Node
-{
-    ll dist;
-    int u;
-    int nexte;
 
-    Node(int u, ll dist, int nexte) : u(u), dist(dist), nexte(nexte) {}
-
-    bool operator<(const Node &b) const
-    {
-        return dist > b.dist;
-    }
-};
-int vlen, elen, qlen;
-ll ans[MAXQ];
-
-void solve()
-{
-    priority_queue<Node> pq;
-    for (int u = 1; u <= vlen; u++)
-    {
-        for (int ei = 0; ei < min((size_t)1, g[u].size()); ei++)
-        {
-            Edge &e = edges[g[u][ei]];
-            pq.push(Node(u, e.w, ei));
-        }
-    }
-    int kth = 0;
-    while (!pq.empty() && kth<=5e4+10)
-    {
-        Node cur = pq.top();
-        ans[++kth]=cur.dist;
-        pq.pop();
-
-        Edge &e = edges[g[cur.u][cur.nexte]];
-        //expand next v
-        Node next = cur;
-        next.u = e.v;
-        next.nexte = 0;
-        bool hasnext = 0;
-        if (next.nexte < g[next.u].size())
-        {
-            next.dist += edges[g[next.u][next.nexte]].w;
-            pq.push(next);
-        }
-
-        bool hascur = 0;
-        // no edge to expand current v
-        if (cur.nexte + 1 < g[cur.u].size())
-        {
-            Edge &ne = edges[g[cur.u][cur.nexte + 1]];
-            cur.dist -= e.w;
-            cur.dist += ne.w;
-            cur.nexte++;
-            pq.push(cur);
+ll S[MAXN];
+void init_S(int len){
+    for(int i=1;i<=len;i++){
+        for(int l=1,r;l<=i;l=r+1){
+            r=i/(i/l);
+            S[i]+=(ll)(r-l+1)*(i/l);
         }
     }
 }
-
-int main()
-{
-    int kase;
-    scanf("%d",&kase);
-    while (kase--)
-    {
-        idx = 0;
-        scanf("%d%d%d",&vlen,&elen,&qlen);
-        for (int i = 0; i <= vlen; i++)
-            g[i].clear();
-
-        for (int i = 1; i <= elen; i++)
-        {
-            int u, v, w;
-            scanf("%d%d%d",&u,&v,&w);
-            adde(u, v, w);
+int main(){
+    ios::sync_with_stdio(false);
+    init_prime(50000);
+    init_S(50000);
+    int kase;cin>>kase;
+    while(kase--){
+        int n,m;cin>>n>>m;
+        ll ans=0;
+        int minn=min(n,m);
+        for(int l=1,r;l<=minn;l=r+1){
+            r=min(n/(n/l),m/(m/l));
+            ans+=S[n/l]*S[m/l]*(premu[r]-premu[l-1]);
         }
-        for (int i = 1; i <= vlen; i++)
-        {
-            sort(g[i].begin(), g[i].end(), [](int a, int b) {
-                return edges[a].w < edges[b].w;
-            });
-        }
-        solve();
-
-        for (int i = 1; i <= qlen; i++)
-        {
-            int q;
-            scanf("%d",&q);
-            printf("%lld\n",ans[q]);
-        }
+        cout<<ans<<endl;
     }
+
     return 0;
 }
