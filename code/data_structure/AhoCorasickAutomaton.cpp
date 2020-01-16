6bcc56563611d2d33f73faa0c86fef19270039ca
Kanari <iovo7c@gmail.com>
2020-01-16 12:21:10 +0800

    fix bugs in ac automaton.
    test
    pattern:a,bab
    string: bab
    last version will fail this test.

---
+++ code/data_structure/AhoCorasickAutomaton.cpp
@@ -0,0 +1,71 @@
+//AC 自动机
+//用于进行多模式匹配
+// #include <cstring>
+// #include <queue>
+// using namespace std;
+struct AhoCorasickAutomaton {
+	struct Node {
+		Node *son[26],*fail,*last;
+		int cnt;
+
+		Node() {
+			memset(son,0,sizeof(son));
+			fail=last=0;
+			cnt=0;
+		}
+		
+	}*root;
+
+	void Insert(char *s) {
+		Node *pos=root;
+		for(;*s;++s) {
+			int c=*s-'a';
+			if(!pos->son[c])
+				pos->son[c]=new Node;
+			pos=pos->son[c];
+		}
+		pos->cnt++;
+	}
+
+	void Build() {
+		root->fail=root->last=root;
+		std::queue<Node*> Q;
+		for(int c=0;c<26;++c)
+			if(root->son[c]) {
+				root->son[c]->fail=root->son[c]->last=root;
+				Q.push(root->son[c]);
+			} else root->son[c]=root;
+		while(!Q.empty()) {
+			Node *cur=Q.front();Q.pop();
+			for(int c=0;c<26;++c)
+				if(cur->son[c]) {
+					Node *u=cur->son[c];
+					u->fail=cur->fail->son[c];
+					u->last=u->fail->cnt?u->fail:u->fail->last;
+					Q.push(u);
+				} else cur->son[c]=cur->fail->son[c];
+		}
+	}
+
+	int Calc(Node *pos) {
+		int res=0;
+		while(pos->last!=pos) {
+			res+=pos->cnt;
+			pos->cnt=0;//该自动机计算匹配的模式串数(不重复计数),删掉后可重复计数
+			pos=pos->last;
+		}
+		return res;
+	}
+
+	int Match(char *s) {
+		Node *pos=root;
+		int res=0;
+		for(;*s;++s) {
+			int c=*s-'a';
+			pos=pos->son[c];
+			res+=Calc(pos);
+		}
+		return res;
+	}
+};
+
